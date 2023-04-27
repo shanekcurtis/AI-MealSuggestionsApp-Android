b688167a2cfa915b38c1566c5caba2f4c7e50316
Boblishka <omajdoub1113@gmail.com>
2023-04-27 17:44:39 -0400

    Add Data types

---
+++ app/src/main/java/com/example/mymealsuggestionapp/model/Ingredient.kt
@@ -0,0 +1,3 @@
+package com.example.mymealsuggestionapp.model
+
+class Ingredient(val name: String, val quantity: Double, val unit: String, val preparation: String)
---
+++ app/src/main/java/com/example/mymealsuggestionapp/model/MealSuggestion.kt
@@ -0,0 +1,7 @@
+package com.example.mymealsuggestionapp.model
+
+class MealSuggestion     // Initialize as not favorite by default
+    (var mealName: String, var ingredients: List<String>) {
+    var isFavorite = false
+
+}
