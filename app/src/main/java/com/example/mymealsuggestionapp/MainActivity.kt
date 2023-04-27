94f862eb471f685ab6679918a888a42467e61eb3
Boblishka <omajdoub1113@gmail.com>
2023-04-27 13:14:15 -0400

    XML files commit

--- app/src/main/java/com/example/mymealsuggestionapp/MainActivity.kt
+++ app/src/main/java/com/example/mymealsuggestionapp/MainActivity.kt
@@ -1,35 +1,31 @@
 package com.example.mymealsuggestionapp
 
+import androidx.navigation.ui.AppBarConfiguration.Builder
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import com.example.mymealsuggestionapp.R
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.NavController
 import com.example.mymealsuggestionapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
+        setContentView(binding!!.root)
+        val navView = findViewById<BottomNavigationView>(R.id.nav_view)
+        val appBarConfiguration: AppBarConfiguration = Builder(
+            R.id.navigation_home,
+            R.id.navigation_Meal,
+            R.id.navigation_save_Meal,
+            R.id.navigation_setting
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+            .build()
+        val navController = findNavController(this, R.id.nav_host_fragment_activity_main)
+        setupWithNavController(binding!!.navView, navController)
     }
 }
